b42505395cf5eafc74183888bcbd1a974b0388e4
NotCclo <marcelosg909@gmail.com>
2024-07-05 07:25:17 -0300

    first commit

---
+++ src/pages/data/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return(
+    <div className='container'>
+      <header>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </header>
+    </div>
+  )
+}
+
+export default App
